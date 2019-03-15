7a79a88369805da57b10f841f0fd9fd923fac2b8
Quentin Yahia <relou0133@gmail.com>
2019-03-15 23:25:57 +0100

    suppression du test

--- controllers/ConnexionController.js
+++ controllers/ConnexionController.js
@@ -1,10 +1,10 @@
 // ////////////////////////////////////////////// C O N N E X I O N
 module.exports.Index = function(request, response){
   response.title = "Administration SIXVOIX";
-  response.render('connexion', response);
+  response.render('home', response);
 };
 
 module.exports.NotFound = function(request, response){
   response.title = "Administration SIXVOIX";
-  response.render('notFoundAdmin', response);
+  response.render('notFound', response);
 };
